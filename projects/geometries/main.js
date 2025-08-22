81a49825526fa994215fcb31439bd4746d677ebb
Sam Strike <sam.strike1@gmail.com>
2025-08-22 23:07:13 +0100

    Added planet textures and fixed double light error.

--- projects/geometries/main.js
+++ projects/geometries/main.js
@@ -7,12 +7,18 @@ const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
+
 document.body.appendChild(renderer.domElement);
+renderer.outputColorSpace = THREE.SRGBColorSpace;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-scene.add(directionalLight);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+scene.add(ambientLight);
+
+const sunLight = new THREE.DirectionalLight(0xffffff, 0.2);
+sunLight.position.set(50,0,50);
+scene.add(sunLight);
 
 // Resize handling
 window.addEventListener('resize', () => {
@@ -22,66 +28,73 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-const light = new THREE.AmbientLight(0xffffff, 5);
-scene.add(light);
 
+const textureLoader = new THREE.TextureLoader();
 
 const planetData = [
     {
         name: "Mercury",
         radius: 2,
         x: 20,
-        orbitSpeed: 0.01,
-        spinSpeed: 0.01
+        orbitSpeed: 0.001,
+        spinSpeed: 0.01,
+        texture: "textures/mercury.jpg"
     },
     {
         name: "Venus",
         radius: 4,
         x: 70,
-        orbitSpeed: 0.02,
-        spinSpeed: 0.01
+        orbitSpeed: 0.002,
+        spinSpeed: 0.01,
+        texture: "textures/venus.jpg"
     },
     {
         name: "Earth",
         radius: 8,
         x: 120,
-        orbitSpeed: 0.03,
-        spinSpeed: 0.01
+        orbitSpeed: 0.003,
+        spinSpeed: 0.01,
+        texture: "textures/earth.jpg"
     },
     {
         name: "Mars",
         radius: 6,
         x: 160,
-        orbitSpeed: 0.04,
-        spinSpeed: 0.01
+        orbitSpeed: 0.004,
+        spinSpeed: 0.01,
+        texture: "textures/mars.jpg"
     },
     {
         name: "Jupiter",
         radius: 20,
         x: 210,
-        orbitSpeed: 0.05,
-        spinSpeed: 0.01
+        orbitSpeed: 0.005,
+        spinSpeed: 0.01,
+        texture: "textures/jupiter.jpg"
     },
     {
         name: "Saturn",
         radius: 15,
         x: 260,
-        orbitSpeed: 0.06,
-        spinSpeed: 0.01
+        orbitSpeed: 0.006,
+        spinSpeed: 0.01,
+        texture: "textures/saturn.jpg"
     },
     {
         name: "Uranus",
         radius: 12,
         x: 310,
-        orbitSpeed: 0.07,
-        spinSpeed: 0.01
+        orbitSpeed: 0.007,
+        spinSpeed: 0.01,
+        texture: "textures/uranus.jpg"
     },
     {
         name: "Neptune",
         radius: 10,
         x: 360,
-        orbitSpeed: 0.08,
-        spinSpeed: 0.01
+        orbitSpeed: 0.008,
+        spinSpeed: 0.01,
+        texture: "textures/neptune.jpg"
     },
 ]
 
@@ -91,9 +104,11 @@ const planets = [];
 planetData.forEach(planet => {
     const orbitGroup = new THREE.Group();
 
+    const texture = textureLoader.load(planet.texture);
+    texture.colorSpace = THREE.SRGBColorSpace;
 
     const geometry = new THREE.SphereGeometry(planet.radius, 15, 15);
-    const material = new THREE.MeshStandardMaterial({ color: 'green', wireframe: true });
+    const material = new THREE.MeshStandardMaterial({ map: texture });
     const mesh = new THREE.Mesh(geometry,material);
 
     mesh.position.x = planet.x;
